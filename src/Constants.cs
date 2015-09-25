930839653f24d8eb9f7af68e74b0c19a04f4c1d9
SimoPrG <simeon_petev@hotmail.com>
2015-09-25 20:51:02 +0300

    Introduced static class Constants

---
+++ src/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace GameFifteen
+{
+    internal static class Constants
+    {
+        public const int MatrixLength = 4;
+        public const string HorizontalBorder = " -------------";
+        public const string VerticalBorder = "|";
+    }
+}
