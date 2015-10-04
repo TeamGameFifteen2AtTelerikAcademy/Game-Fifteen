47d2650c63767878866db1a50b16dc6135071a26
iliantova <iliyana.antova@gmail.com>
2015-10-04 23:38:52 +0300

    Tests_Common/ValidatorTest.cs throw exceptions in methods

---
+++ src/Tests_Common/ValidatorTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameFifteen.Logic.Common;
+
+namespace Tests_Common
+{
+    [TestClass]
+    public class ValidatorTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException),
+          "Argument name cannot be null!")]
+        public void NullArgumentInMehodValidateIsNotNull()
+        {
+            Validator.ValidateIsNotNull(null, Constants.ArgumentName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),
+          "Parametar must be a positive integer")]
+        public void NegativeIntegerLikeParameterInMehodValidateIsPositiveInteger()
+        {
+            int numberNegative = -1;
+            string name = "Parametar";
+            Validator.ValidateIsNotNull(numberNegative, name);
+        }
+        
+    }
+}
