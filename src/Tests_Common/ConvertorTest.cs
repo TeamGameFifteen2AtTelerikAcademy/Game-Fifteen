b4d7a1c113bac9e8ae9b77f1c0ea7dbf3463bdf9
iliantova <iliyana.antova@gmail.com>
2015-10-05 23:50:29 +0300

    class ConvertorTest

---
+++ src/Tests_Common/ConvertorTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameFifteen.Logic.Common;
+
+namespace Tests_Common
+{
+    /// <summary>
+    /// Summary description for ConvertorTest
+    /// </summary>
+    [TestClass]
+    public class ConvertorTest
+    {
+        [TestMethod]
+        public void TestConvertLettersToNumber()
+        {
+            int numberForCheck = 65;
+            string letterForConvert = "A";
+            int expectNumber = Converter.ConvertLettersToNumber(letterForConvert);
+            Assert.AreEqual(numberForCheck, expectNumber);
+            
+        }
+
+        [TestMethod]
+        public void TestConvertNumberToLetters()
+        {
+            string letterForCheck = "a";
+            int numberForConvert = 97;
+            string expectLetter = Converter.ConvertNumberToLetters(numberForConvert);
+            Assert.AreEqual(letterForCheck, expectLetter);
+
+        }
+
+    }
+}
