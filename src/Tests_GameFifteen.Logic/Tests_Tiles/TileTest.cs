2e6179d18acc8314cb27efccd27bc3b02392fab5
iliantova <iliyana.antova@gmail.com>
2015-10-08 00:05:21 +0300

    Test override Equal Method from Tile

---
+++ src/Tests_GameFifteen.Logic/Tests_Tiles/TileTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using GameFifteen.Logic.Tiles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests_GameFifteen.Logic.Tests_Tiles
+{
+    /// <summary>
+    /// Summary description for TileTest
+    /// </summary>
+    [TestClass]
+    public class TileTest
+    {
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        
+        [TestMethod]
+        public void ExpectTrueEqualsMethodWhithTwoITileObjeckTest()
+        {
+            var numberTileFirst = new NumberTile(1);
+            var numberTileSecond = new NumberTile(1);
+            bool result = numberTileFirst.Equals(numberTileSecond);
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public void ExpectTrueEqualsMethodWhithOneITileObjeckAndObjecktTest()
+        {
+            var numberTileFirst = new NumberTile(1);
+            var numberTileSecond = new NumberTile(1);
+            bool result = numberTileFirst.Equals(numberTileSecond as Object);
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public void ExpectFalseEqualsMethodWhithNullObjeckTest()
+        {
+            var numberTileFirst = new NumberTile(1);
+            NumberTile numberTileSecond = null;
+            bool result = numberTileFirst.Equals(numberTileSecond);
+            Assert.IsFalse(result);
+
+        }
+    }
+}
