23e9a79a3c69142f678949bc7f6751251118dd96
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-10-09 04:20:23 +0300

    Tests for Engine[100%]

---
+++ src/GameFifteen.Tests/Tests_UI.Console/EngineTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using GameFifteen.Logic.Memento;
+using GameFifteen.Logic.Scoreboards;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameFifteen.UI.Console;
+
+namespace GameFifteen.Tests.UI.Console
+{
+    [TestClass]
+    public class EngineTests
+    {
+        [TestMethod]
+        public void ExpectEngineToRunProperlyAndForcedExitAfterTwoSteps()
+        {
+            var reader = MockStorage.GetReader("exit");
+
+            var engine = new Engine(
+
+                MockStorage.GetGame(),
+                new Scoreboard(),
+                MockStorage.GetPrinter(),
+                reader,
+                new CommandManager(),
+                new BoardHistory()
+
+                );
+            
+            engine.Run();
+        }
+
+        [TestMethod]
+        public void ExpectToExecuteGameOverWhenGameIsSolved()
+        {
+            var reader = MockStorage.GetReader("exit");
+
+            var engine = new Engine(
+
+                MockStorage.GetSolvedGame(),
+                new Scoreboard(),
+                MockStorage.GetPrinter(),
+                reader,
+                new CommandManager(),
+                new BoardHistory()
+
+                );
+
+            engine.Run();
+        }
+    }
+}
---
+++ src/GameFifteen.Tests/Tests_UI.Console/MockStorage.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using GameFifteen.Logic.Commands;
+using GameFifteen.Logic.Frames;
+using GameFifteen.Logic.Frames.Contracts;
+using GameFifteen.Logic.Tiles.Contracts;
+using GameFifteen.UI.Console;
+
+namespace GameFifteen.Tests.UI.Console
+{
+    using Logic.Games.Contracts;
+    using Moq;
+
+    public class MockStorage
+    {
+        public static IPrinter GetPrinter()
+        {
+            var mockedPriner = new Mock<IPrinter>();
+
+            mockedPriner.Setup(x => x.Print(It.IsAny<object>()))
+                .Verifiable();
+
+            mockedPriner.Setup(x => x.PrintLine(It.IsAny<object>()))
+                .Verifiable();
+            return mockedPriner.Object;
+        }
+
+        public static IReader GetReader(string value)
+        {
+            var mockedReader = new Mock<IReader>();
+            var numberOfCalls = 2;
+            mockedReader.Setup(x => x.ReadLine())
+                .Callback(() =>
+                {
+                    if (numberOfCalls == 0)
+                    {
+                        value = "Exit";
+                    }
+                    else
+                    {
+                        value = It.IsAny<string>();
+                        numberOfCalls--;
+
+                    }
+                })
+                .Returns(value);
+
+            mockedReader.Setup(x => x.ParseInput(It.IsAny<string>()))
+                .Returns(new Reader().ParseInput(value));
+
+            return mockedReader.Object;
+        }
+
+        public static IGame GetGame()
+        {
+            var mockedGame = new Mock<IGame>();
+            var numberOfCalls = 2;
+            bool isOver = false;
+
+
+            mockedGame.Setup(x => x.Move(It.IsAny<string>()))
+                .Returns(true);
+
+            mockedGame.Setup(x => x.Shuffle())
+                .Verifiable();
+
+            mockedGame.Setup(x => x.Frame)
+                .Returns(new Frame(new ITile[3, 3]));
+            
+            return mockedGame.Object;
+        }
+
+        public static IGame GetSolvedGame()
+        {
+            var mockedGame = new Mock<IGame>();
+            var numberOfCalls = 2;
+            bool isOver = false;
+
+
+            mockedGame.Setup(x => x.Move(It.IsAny<string>()))
+                .Returns(true);
+
+            mockedGame.Setup(x => x.Shuffle())
+                .Verifiable();
+
+            mockedGame.Setup(x => x.Frame)
+                .Returns(new Frame(new ITile[3, 3]));
+
+            mockedGame.Setup(x => x.IsSolved)
+                .Returns(true);
+
+            return mockedGame.Object;
+        }
+
+        public static ICommandManager GeCommandManager()
+        {
+            var mockedManager = new Mock<ICommandManager>();
+
+
+
+            return mockedManager.Object;
+        }
+    }
+}
