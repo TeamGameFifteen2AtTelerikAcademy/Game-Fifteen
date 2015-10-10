1c01b7b1099957d91bb2adfaf5fa65d7e7398b08
Ivo Paounov <ivo.paunov@gmail.com>
2015-10-10 22:58:07 +0300

    Movers - NO stylecop warnings.

--- src/GameFifteen.Logic/Movers/Contracts/Mover.cs
+++ src/GameFifteen.Logic/Movers/Contracts/Mover.cs
@@ -1,4 +1,13 @@
-﻿namespace GameFifteen.Logic.Movers.Contracts
+﻿// <copyright file="Mover.cs" company="GameFifteen2Team">
+// The MIT License (MIT)
+// Copyright(c) 2015 Team "Game-Fifteen-2"
+// </copyright>
+// <summary>
+// Abstract class Mover.
+// </summary>
+// <author>GameFifteen2Team</author>
+
+namespace GameFifteen.Logic.Movers.Contracts
 {
     using System;
     using System.Collections.Generic;
@@ -10,8 +19,17 @@
     /// </summary>
     public abstract class Mover : IMover
     {
+        /// <summary>
+        /// Holds object of class Position.
+        /// </summary>
         protected readonly Position NotFoundPosition = new Position(-1, -1);
 
+        /// <summary>
+        /// Moves tile in frame by given tile label. And returns if move is successful.
+        /// </summary>
+        /// <param name="tileLabel">Tile of type ITile.</param>
+        /// <param name="frame">Frame of type IFrame.</param>
+        /// <returns>True or false.</returns>
         public virtual bool Move(string tileLabel, IFrame frame)
         {
             var tilePosition = this.FindTilePosition(tileLabel, frame);
@@ -25,6 +43,10 @@
                 this.NotFoundPosition == nullTilePosition);
         }
 
+        /// <summary>
+        /// Shuffles the frame.
+        /// </summary>
+        /// <param name="frame">Frame to  be shuffled.</param>
         public void Shuffle(IFrame frame)
         {
             var random = new Random();
@@ -40,6 +62,12 @@
             }
         }
 
+        /// <summary>
+        /// Finds tile position.
+        /// </summary>
+        /// <param name="tileLabel">Tile label.</param>
+        /// <param name="frame">The frame.</param>
+        /// <returns>Founded Position.</returns>
         protected Position FindTilePosition(string tileLabel, IFrame frame)
         {
             for (int row = 0; row < frame.Rows; row++)
@@ -56,6 +84,12 @@
             return this.NotFoundPosition;
         }
 
+        /// <summary>
+        /// Swaps to tiles by their positions.
+        /// </summary>
+        /// <param name="frame">Frame that holds the tiles.</param>
+        /// <param name="firstTilePosition">First tile.</param>
+        /// <param name="secondTilePosition">Second tile.</param>
         protected void SwapTwoTilesInFrameByPosition(IFrame frame, Position firstTilePosition, Position secondTilePosition)
         {
             var firstTile = frame.Tiles[firstTilePosition.Row, firstTilePosition.Col];
@@ -66,6 +100,11 @@
             frame.Tiles[secondTilePosition.Row, secondTilePosition.Col] = firstTile;
         }
 
+        /// <summary>
+        /// Abstract method that returns current movable tiles labels.
+        /// </summary>
+        /// <param name="frame">Frame of type IFrame.</param>
+        /// <returns>List of strings.</returns>
         protected abstract List<string> GetCurrentMovableTileLabels(IFrame frame);
 
         /// <summary>
@@ -73,38 +112,73 @@
         /// </summary>
         protected class Position
         {
+            /// <summary>
+            /// Initializes a new instance of the Position class.
+            /// </summary>
+            /// <param name="row">The position row.</param>
+            /// <param name="col">The position column.</param>
             public Position(int row, int col)
             {
                 this.Row = row;
                 this.Col = col;
             }
 
+            /// <summary>
+            /// Gets the row.
+            /// </summary>
+            /// <value>Row of type integer.</value>
             public int Row
             {
                 get; private set;
             }
 
+            /// <summary>
+            /// Gets the column.
+            /// </summary>
+            /// <value>Column of type integer.</value>
             public int Col
             {
                 get; private set;
             }
 
+            /// <summary>
+            /// Override the default operator == behavior.
+            /// </summary>
+            /// <param name="firstPosition">First Position.</param>
+            /// <param name="secondPosition">Second Position.</param>
+            /// <returns>True or false.</returns>
             public static bool operator ==(Position firstPosition, Position secondPosition)
             {
                 return object.Equals(firstPosition, secondPosition);
             }
 
+            /// <summary>
+            /// Override the default operator != behavior.
+            /// </summary>
+            /// <param name="firstPosition">First Position.</param>
+            /// <param name="secondPosition">Second Position.</param>
+            /// <returns>True or false.</returns>
             public static bool operator !=(Position firstPosition, Position secondPosition)
             {
                 return !object.Equals(firstPosition, secondPosition);
             }
 
+            /// <summary>
+            /// Override the default Equals method.
+            /// </summary>
+            /// <param name="obj">The object to be compared with.</param>
+            /// <returns>True or false.</returns>
             public override bool Equals(object obj)
             {
                 var other = obj as Position;
                 return this.Equals(other);
             }
 
+            /// <summary>
+            /// Override the default Equals method.
+            /// </summary>
+            /// <param name="other">The object to be compared with.</param>
+            /// <returns>True or false.</returns>
             public bool Equals(Position other)
             {
                 if (other == null)
@@ -115,6 +189,10 @@
                 return this.Row == other.Row && this.Col == other.Col;
             }
 
+            /// <summary>
+            /// Override the default GetHashCode method.
+            /// </summary>
+            /// <returns>Unique integer number.</returns>
             public override int GetHashCode()
             {
                 return (this.Row ^ (this.Col << 16)).GetHashCode();
