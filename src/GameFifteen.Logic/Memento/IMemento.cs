2cd9dfa01ed6782cb153bcfb6fc37c1529d45e29
Ivo Paounov <ivo.paunov@gmail.com>
2015-10-10 22:38:22 +0300

    Memento - No stylecop warnings.

--- src/GameFifteen.Logic/Memento/IMemento.cs
+++ src/GameFifteen.Logic/Memento/IMemento.cs
@@ -1,13 +1,36 @@
-﻿namespace GameFifteen.Logic.Memento
+﻿// <copyright file="IMemento.cs" company="GameFifteen2Team">
+// The MIT License (MIT)
+// Copyright(c) 2015 Team "Game-Fifteen-2"
+// </copyright>
+// <summary>
+// Interface for IMemento.
+// </summary>
+// <author>GameFifteen2Team</author>
+
+namespace GameFifteen.Logic.Memento
 {
     using GameFifteen.Logic.Frames.Contracts;
 
+    /// <summary>
+    /// Interface for IMemento.
+    /// </summary>
     public interface IMemento
     {
+        /// <summary>
+        /// Saves board state.
+        /// </summary>
+        /// <param name="board">Board of type IFrame.</param>
         void SaveBoardState(IFrame board);
 
+        /// <summary>
+        /// Clears the history.
+        /// </summary>
         void ClearHistory();
 
+        /// <summary>
+        /// Returns last saved board state.
+        /// </summary>
+        /// <returns>Returns IFrame.</returns>
         IFrame Undo();
     }
 }
