15ab1876c62df3d150e7608863cee810a0277428
iliantova <iliyana.antova@gmail.com>
2015-10-10 12:49:46 +0300

    Added folder for Mover tests

---
+++ src/Tests_GameFifteen.Logic/Tests_Mover/Mover.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests_GameFifteen.Logic.Tests_Mover
+{
+    /// <summary>
+    /// Summary description for Mover
+    /// </summary>
+    [TestClass]
+    public class Mover
+    {
+       
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+       
+        [TestMethod]
+        public void TestMethod1()
+        {
+          
+        }
+    }
+}
