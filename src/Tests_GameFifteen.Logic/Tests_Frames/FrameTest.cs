dc001ac54733ba0138105a293a3ffb57b613f820
iliantova <iliyana.antova@gmail.com>
2015-10-10 11:40:43 +0300

    Frame tests Equals method

--- src/Tests_GameFifteen.Logic/Tests_Frames/FrameTest.cs
+++ src/Tests_GameFifteen.Logic/Tests_Frames/FrameTest.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
+using GameFifteen.Logic.Frames;
+using GameFifteen.Logic.Frames.Contracts;
+using GameFifteen.Logic.Tiles;
+using GameFifteen.Logic.Tiles.Contracts;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests_GameFifteen.Logic.Tests_Frames
@@ -15,11 +19,35 @@ namespace Tests_GameFifteen.Logic.Tests_Frames
         ///Gets or sets the test context which provides
         ///information about and functionality for the current test run.
         ///</summary>
-       
+
         [TestMethod]
-        public void TestMethod1()
+        public void ExpectTrueEqualsMethodWhithTwoIframeTest()
         {
-            
+            TileFactory tileFactoryFirst = new NumberTileFactory();
+            TileFactory tileFactorySecond = new NumberTileFactory();
+            FrameBuilder frameBuilderFirst = new ClassicPatternFrameBuilder(tileFactoryFirst);
+            FrameBuilder frameBuilderSecond = new ClassicPatternFrameBuilder(tileFactorySecond);
+            var frameDirectorFirst = new FrameDirector(frameBuilderFirst);
+            var frameDirectorSecond = new FrameDirector(frameBuilderSecond);
+            var expectetFrameFirst = frameDirectorFirst.ConstructFrame(5, 5);
+            var expectetFrameSecond = frameDirectorSecond.ConstructFrame(5, 5);
+            bool result = expectetFrameFirst.Equals(expectetFrameSecond);
+            Assert.IsTrue(result);
+
         }
+
+        [TestMethod]
+        public void ExpectTrueEqualsMethodWhithNullIframeTest()
+        {
+            TileFactory tileFactoryFirst = new NumberTileFactory();
+            FrameBuilder frameBuilderFirst = new ClassicPatternFrameBuilder(tileFactoryFirst);
+            var frameDirectorFirst = new FrameDirector(frameBuilderFirst);
+            var expectetFrameFirst = frameDirectorFirst.ConstructFrame(5, 5);
+            IFrame expectetFrameSecond = null;
+            bool result = expectetFrameFirst.Equals(expectetFrameSecond);
+            Assert.IsFalse(result);
+
+        }
+
     }
 }
