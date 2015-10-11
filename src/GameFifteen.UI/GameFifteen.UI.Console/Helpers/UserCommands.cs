ea8d52fc2b6972c9012a027b4e21a6f8e9ca613a
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-10-11 13:42:48 +0300

    Added Invalid state to UserCommands, so will not be needed to create NEW invalid commnad every time

--- src/GameFifteen.UI/GameFifteen.UI.Console/Helpers/UserCommands.cs
+++ src/GameFifteen.UI/GameFifteen.UI.Console/Helpers/UserCommands.cs
@@ -37,6 +37,11 @@ namespace GameFifteen.UI.Console
         /// <summary>
         /// Move command.
         /// </summary>
-        Move
+        Move,
+
+        /// <summary>
+        /// Invalid command.
+        /// </summary>
+        Invalid
     }
 }
